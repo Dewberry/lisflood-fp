8679689754b762405eab33d745a400bc5a7e3aa0
ShaneMPutnam <sputnam@Dewberry.com>
2025-11-11 21:22:47 +0000

    Add run model endpoint and check post body

--- api/controller/models.go
+++ api/controller/models.go
@@ -6,8 +6,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Model struct {
+	ModelDir string `json:"model_dir"`
+}
+
 func (ctrl *Controller) HandleRunModel(c echo.Context) error {
 
-	return c.JSON(http.StatusOK, map[string]string{"message": "healthy"})
+	var b Model
+	if err := c.Bind(&b); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": err.Error()})
+	}
+	if b.ModelDir == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Missing the following required field: model_dir"})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "Recieved model request for: " + b.ModelDir})
 
 }
